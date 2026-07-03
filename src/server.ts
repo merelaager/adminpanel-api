60aea7af4adc1bb17ebc66413ae47e4e48deddcf
Taka <71265675+takakv@users.noreply.github.com>
2026-07-03 20:45:28 +0300

    Shut down gracefully

--- src/server.ts
+++ src/server.ts
@@ -74,3 +74,17 @@ const start = async () => {
 };
 
 start();
+
+const closeGracefully = async (signal: NodeJS.Signals) => {
+  fastify.log.info(`Received ${signal}, shutting down gracefully`);
+  try {
+    await fastify.close();
+    process.exit(0);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+process.on("SIGTERM", closeGracefully);
+process.on("SIGINT", closeGracefully);
